064df11f87ab8d47cd320b2ad57630b8526dd214
viviier <774593324@qq.com>
2017-02-27 23:38:30 +0800

    footer

--- react-necedu/src/index.js
+++ react-necedu/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {render} from 'react-dom'
 import Header from './components/Header/Header'
 import Content from './components/Content/Content'
+import Footer from './components/Footer/Footer'
 import './styles.less'
 
 
@@ -9,6 +10,7 @@ const App = () => (
 	<div>
 		<Header />
 		<Content />
+		<Footer />
 		</div>
 )
 
